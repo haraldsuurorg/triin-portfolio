965b0894f68f663ccac5e4edf04035cecaa7eb42
Harald Suurorg <harald.suurorg@gmail.com>
2026-03-07 08:38:26 +0200

    Remove next config routing

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return {
-            beforeFiles: [
-                {
-                    source: '/',
-                    destination: '/et',
-                },
-                {
-                    source: '/:path((?!api|_next|_vercel|et|en|.*\\..*).*)',
-                    destination: '/et/:path',
-                },
-            ],
-        };
-    },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
